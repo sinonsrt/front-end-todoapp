ef86e87075df8dab35e13b0baac0dabeebb18190
Lucas Marchiori <sinonsrt@hotmail.com>
2021-11-25 22:12:46 -0300

    Config route

---
+++ src/themes/theme.tsx
@@ -0,0 +1,21 @@
+import { ThemeProvider } from 'styled-components';
+
+const fontSizes: any = [14, 20, 96];
+fontSizes.body = fontSizes[0];
+fontSizes.bodyExtraLarge = fontSizes[1];
+fontSizes.displayExtraLarge = fontSizes[2];
+
+const primary = '#2567B4';
+const secondary = '#F9B531';
+
+const theme = {
+  fontSizes,
+  colors: {
+    primary,
+    secondary
+  },
+};
+
+export const Theme: React.FC = ({ children }) => {
+  return <ThemeProvider theme={theme}></ThemeProvider>;
+};
